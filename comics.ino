ac27d393d6ab78cc052c6b7e6157e34399e41599
Stephan Lohse <dev-github@ploek.org>
2020-12-19 10:42:36 +0100

    partialUpdate doesn't do anything (at least not in combination with println)

--- comics.ino
+++ comics.ino
@@ -8,21 +8,28 @@ void setup() {
     display.begin();
     display.clearDisplay(); // clear buffer
     display.clean(); // clear screen
+    display.display();
+
+    display.setTextColor(0,7);
+    display.setCursor(0, 5);
+    display.setTextSize(2);
+
+    display.println("booting...");
 
     // Init SD card. Handle if SD card could not be accessed
     if (display.sdCardInit())
     {
-        display.println("SD Card OK!");
-        display.partialUpdate();
+        display.println("SD Card OK.");
     }
     else
     {
-        display.println("SD Card error!");
-        display.partialUpdate();
+        display.println("SD Card error.");
+        display.display();
         while (true) {
             // nop 
         }
     }
+    display.display();
     delay(1000);
 
 }
