eb0db095cd840f9c8aee143701ecd6e86c20b188
Stephan Lohse <dev-github@ploek.org>
2022-01-26 20:08:56 +0100

    add some doxygen to ViewIF

--- src/modules/ViewIF.h
+++ src/modules/ViewIF.h
@@ -9,14 +9,36 @@
 
 #include "../lib/toml.h"
 
+/**
+ * Interface that all view modules need to implement
+ */
 class ViewIF {
 public:
+    /**
+     * Initializes the module
+     *
+     * This function is meant to do all the initialization needed by the module
+     *
+     * @param display Reference to the Inkplate object that allows the module to draw on the screen
+     * @param cfg Pointer to the TOML table that defines this module
+     */
     virtual bool setup(Inkplate &display, toml_table_t *cfg);
 
+    /**
+     * Function that gets called when the view becomes active
+     *
+     * This function is called when the middle button is pressed and the view gets cycled in as the active view.
+     */
     virtual void resume();
 
+    /**
+     * Function that gets called when the left button is pressed
+     */
     virtual void leftButton();
 
+    /**
+     * Function that gets called when the right button is pressed
+     */
     virtual void rightButton();
 };
 
